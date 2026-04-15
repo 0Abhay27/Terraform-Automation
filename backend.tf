0ef69af0170d36cec20ab3d8da4a2a3b2e119975
0Abhay27 <0abhay1999@gmail.com>
2026-04-15 23:05:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "app-project-terraform-state-27"
+    bucket = "app-project-terraform-state-2"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
